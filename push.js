ca9efb36c3ee0f30b1d693dc5379a89dd359da4a
jonathanhflores <jhanzen@scale.com.br>
2022-08-01 15:13:50 -0300

    index

--- push.js
+++ push.js
@@ -6,5 +6,5 @@ let vapidKeys = { publicKey:
 
 push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let subscription = {"endpoint":"https://wns2-bl2p.notify.windows.com/w/?token=BQYAAAD3j4ECyhEy8PPAiCYrbiqSlH4RQBkVVjwWs69LNmEElENxRWZw9YK6J5UBChSx39qFcqyb4%2ftATh1VCm2fiTsmlHORMQS2JSpVrg5pmDTbVHQ%2buG5%2fjcU3bNJ0zcJukbxPmIBVfInX%2bdWV8SHzOXqtkVJ%2feQf5POnlcfiR%2f%2flD9WghUdaI6hcwZ1J4d%2bTpK0cwxPvg11YjPwdET4rp6gdRy6WJ%2bFJNuYl3ZjI7%2fJsB8NtZo%2bppujxr8ZEy8F0fk9gkw2J3tSCQ3bwqHcirxNtCdwhLC%2faVufRRkZTbnhNn2CqsC%2fuHgXC90b9kjIE78L6LXMc3J0TV6uiVxA%2b9CyHEqpUB%2ba%2fukTivHkGQY%2fN%2ftA%3d%3d","expirationTime":null,"keys":{"p256dh":"BGHv-VnF2v6KpvdJatxW6uGJKAN90v-YJ2a-0B54r5Io1XS0nd03kHdIWGFOtvTOT2EMmYJ5jHPbGs4pkKOLTHo","auth":"KwjZnRfsaD0dMQYYWwh9Eg"}}
 push.sendNotification(subscription, 'test message');
--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
 self.addEventListener("push", function (event) {
-    const message = event.data.json();
     self.registration.showNotification(
       'test message', {});;
         //  message.title, { body: message.text });
